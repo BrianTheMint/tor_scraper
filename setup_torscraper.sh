f44b41f9b6ba7d1c6a65d862b7b09ad43d9cd742
BrianTheMint <73714723+BrianTheMint@users.noreply.github.com>
2024-12-17 00:22:03 -0500

    Update setup_torscraper.sh

--- setup_torscraper.sh
+++ setup_torscraper.sh
@@ -59,7 +59,7 @@ echo "Activating virtual environment and installing requirements..."
 source "$VENV_DIR/bin/activate"
 pip install --no-cache-dir -r "$REQUIREMENTS_DEST"
 deactivate
-chmod -R 777 /tor_scraper
+sudo chmod -R 777 /tor_scraper
 
 # Restart services to apply changes
 echo "Restarting Tor and Privoxy services..."
