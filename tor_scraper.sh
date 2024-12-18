e646f03d5cf765dd94feb759e1c3acffdecbfd5f
brianTheMint <orlandb204567@outlook.com>
2024-12-18 04:26:00 -0500

    new file:   tor_scraper.sh

---
+++ tor_scraper.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Navigate to the /tor_scraper directory
+cd /tor_scraper
+
+# Activate the virtual environment
+source venv/bin/activate
+
+# Run the Python script
+python3 ./crawler.py
+
+# Deactivate the virtual environment after the script finishes
+deactivate
